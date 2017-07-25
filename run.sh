c1a023cedbe5bdf27ec1459fd7be0f86be9679f1
Slijkhuis <slijkhuis.thijs@gmail.com>
2017-07-25 07:57:01 +0200

    Added executable flags to scripts

--- run.sh
+++ run.sh
@@ -22,9 +22,9 @@ debug "AWS CLI has been configured"
 
 # Register new task revision
 debug "Going to register task $TASKFILE, which is based on template $WERCKER_DEPLOY_TO_ECS_TEMPLATE_FILE"
-aws ecs register-task-definition --cli-input-json "file://$TASKFILE"
+aws ecs register-task-definition --cli-input-json "file://$TASKFILE" || exit 1
 debug "Going to update service in cluster $WERCKER_DEPLOY_TO_ECS_CLUSTER"
-aws ecs update-service --cluster "$WERCKER_DEPLOY_TO_ECS_CLUSTER" --service "$WERCKER_DEPLOY_TO_ECS_SERVICE" --task-definition "$WERCKER_DEPLOY_TO_ECS_TASK"
+aws ecs update-service --cluster "$WERCKER_DEPLOY_TO_ECS_CLUSTER" --service "$WERCKER_DEPLOY_TO_ECS_SERVICE" --task-definition "$WERCKER_DEPLOY_TO_ECS_TASK" || exit 1
 debug "Updated service has been deployed"
 
 # Deregister old tasks (keep the 2 newest versions)
